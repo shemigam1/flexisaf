5311f7dfb9b69dcf68d6e1bb887e3eba2212f9ad
semilore <semilore@omotade-michaels.com>
2024-10-18 07:15:34 +0100

    initial commit

---
+++ week5/README.md
@@ -0,0 +1,27 @@
+# flexisaf api calls demo
+
+## Features
+
+- **GET**: Fetch and display a single post from the JSONPlaceholder API.
+- **POST**: Add new blog posts using predetermined data
+- **PUT**: Update an existing blog post by editing its title and body.
+- **DELETE**: Remove a blog post from the list.
+
+## Installation and Setup
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/shemigam1/flexisaf.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd flexisaf/week4
+   ```
+3. Install the dependencies:
+   ```bash
+   npm i
+   ```
+4. Start the react server:
+   ```bash
+   npm run dev
+   ```
---
+++ week5/src/App.jsx
@@ -0,0 +1,129 @@
+import { useEffect, useRef, useState } from "react"
+import './App.css'
+
+function useSayMyName(name) {
+  return `my name is ${name}`
+}
+
+function App() {
+  const [output, setOutput] = useState({})
+  const [count, setCount] = useState(0)
+  const [ref, setRef] = useState(0)
+  let clicks = useRef(0)
+  const name = useSayMyName('diego')
+
+  const url = 'https://jsonplaceholder.typicode.com/posts/1'
+  const postUrl = 'https://jsonplaceholder.typicode.com/posts'
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(url)
+      const data = await response.json()
+      setOutput({
+        message: 'Data fetched successfully with useEffect on initial render',
+        data: data
+      })
+    }
+    fetchData()
+  }, [])
+
+  const counter = () => {
+    clicks.current = clicks.current + 1
+    setRef(clicks.current)
+    return setCount(count + 1)
+  }
+
+  // const getData = async (method) => {
+  //   switch (method) {
+  //     case 'GET':
+  //       const response = await fetch(url)
+  //       const data = await response.json()
+  //       setOutput({
+  //         message: 'Data fetched successfully',
+  //         data: data
+  //       })
+
+  //       break;
+
+  //     case 'POST':
+  //       const postResponse = await fetch(postUrl, {
+  //         method: 'POST',
+  //         body: JSON.stringify({
+  //           title: 'foo',
+  //           body: 'bar',
+  //           userId: 1,
+  //         }),
+  //         headers: {
+  //           'Content-type': 'application/json; charset=UTF-8',
+  //         },
+  //       })
+  //       const postData = await postResponse.json()
+  //       setOutput({
+  //         message: 'Data posted successfully',
+  //         data: postData
+  //       })
+  //       break;
+
+  //     case 'PUT':
+  //       const putResponse = await fetch(url, {
+  //         method: 'PUT',
+  //         body: JSON.stringify({
+  //           title: 'foo',
+  //         }),
+  //         headers: {
+  //           'Content-type': 'application/json; charset=UTF-8',
+  //         },
+  //       })
+  //       const putData = await putResponse.json()
+  //       setOutput({
+  //         message: 'Data updated successfully',
+  //         data: putData
+  //       })
+  //       break;
+
+  //     case 'DELETE':
+  //       const deleteResponse = await fetch(url, {
+  //         method: 'DELETE',
+  //       })
+  //       const deleteData = await deleteResponse.json()
+  //       setOutput({
+  //         message: 'Data deleted successfully',
+  //       })
+  //       break;
+
+  //     default:
+  //       break;
+  //   }
+  // }
+
+  return (
+    <>
+
+      <div className="container">
+        <h1>HOOKS DEMO</h1>
+        <div className="btns">
+
+          {/* <button onClick={() => getData('GET')}>GET</button> */}
+          {/* <button onClick={() => getData('POST')}>POST</button> */}
+          {/* <button onClick={() => getData('PUT')}>PUT</button> */}
+          {/* <button onClick={() => getData('DELETE')}>DELETE</button> */}
+
+          <button onClick={counter}>count {count}</button>
+          <button onClick={() => alert(name)}>What is my name</button>
+        </div>
+
+        <div className="msg">
+          <p>The count button has been clicked {ref} times</p>
+          <p>{output.message}</p>
+          <p>{output.data && JSON.stringify(output.data)}</p>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+
+
+
+export default App;
